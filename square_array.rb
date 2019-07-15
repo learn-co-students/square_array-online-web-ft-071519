25090e8103ad61a09428cc5ee63589400f110006
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-15 17:29:13 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,18 @@
+=begin 
 def square_array(array)
-  # your code here
+  count = 0 
+  new_array = []
+  while count < array.length do
+   array2 = array[count]
+   new_array << array2**2
+  count += 1
+  end 
+  new_array
 end
+=end
+
+def square_array(array)
+  new_array = []
+  array.each { |element| new_array << element **2 }
+  new_array
+end 
